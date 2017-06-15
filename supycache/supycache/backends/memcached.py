360bbdc61ac06c44aafc93412a900eceab315a44
liuchuanshi <844952162@qq.com>
2017-06-15 10:33:25 +0800

    去掉memcache 增加redis

--- supycache/supycache/backends/memcached.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-import pylibmc
-from .base import BaseCache
-
-
-class MemcachedCache(pylibmc.Client, BaseCache):
-
-    clear = pylibmc.Client.flush_all
--- supycache/supycache/backends/redis_cache.py
+++ supycache/supycache/backends/redis_cache.py
@@ -1,20 +1,27 @@
 import redis
 from .base import BaseCache
 class RedisCache(BaseCache):
-    def __init__(self, config=None):
+    def __init__(self, master="127.0.0.1:6379",salve=None,config=None):
         super(RedisCache, self).__init__(config)
-        self._conn =  redis.Redis(host='127.0.0.1',port=6379)
+        host_info = master.split(":")
+        self._mconn = redis.Redis(host=host_info[0], port=int(host_info[1]))
+        if not salve:
+            self._sconn = self._mconn
+        else:
+            host_info = salve.split(":")
+            self._sconn = redis.Redis(host=host_info[0], port=int(host_info[1]))
+
 
     def get(self,key):
-        return self._conn.get(key)
+        return self._sconn.get(key)
 
     def set(self,key,value):
-        max_age = self.config.get('max_age')
-        self._conn.setex(key,value,max_age)
+        max_age = self.config.get('max_age',100)
+        self._mconn.setex(key,value,max_age)
 
     def delete(self, key):
-        self._conn.delete(key)
+        self._mconn.delete(key)
 
     def clear(self):
-        self._conn.flushdb()
+        self._mconn.flushdb()
 
