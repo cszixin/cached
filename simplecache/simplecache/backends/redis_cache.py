50140ed9561d7b8568ec290be34c901a57e88157
liuchuanshi <liuchuanshi@antiy.cn>
2017-06-16 15:27:20 +0800

    redis connection

--- simplecache/simplecache/backends/redis_cache.py
+++ simplecache/simplecache/backends/redis_cache.py
@@ -8,12 +8,22 @@ class RedisCache(BaseCache):
         # 支持redis的主从设置,写主库,读从库
         super(RedisCache, self).__init__(config)
         host_info = master.split(":")
-        self._mconn = redis.Redis(host=host_info[0], port=int(host_info[1]))
+        password = None
+        if len(host_info) < 2:
+            raise ValueError('redis conn[ip:port:password]')
+        elif len(host_info) == 3:
+            password = host_info[2]
+        self._mconn = redis.Redis(host=host_info[0], port=int(host_info[1]), password=password)
         if not salve:
             self._sconn = self._mconn
         else:
             host_info = salve.split(":")
-            self._sconn = redis.Redis(host=host_info[0], port=int(host_info[1]))
+            password = None
+            if len(host_info) < 2:
+                raise ValueError('redis conn[ip:port:password]')
+            elif len(host_info) == 3:
+               password = host_info[2]
+            self._sconn = redis.Redis(host=host_info[0], port=int(host_info[1]), password=password)
 
     def get(self,key):
         result=self._sconn.get(key)
